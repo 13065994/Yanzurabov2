9c06530fda91ef2eba3370da0295c43514df4629
Hahsgwbhdhdj89i9 <166443104+13065994@users.noreply.github.com>
2024-08-26 10:31:56 +0100

    Update uptime8.js

--- scripts/cmds/uptime8.js
+++ scripts/cmds/uptime8.js
@@ -4,7 +4,7 @@ const exec = util.promisify(require('child_process').exec);
 
 module.exports = {
   config: {
-    name: "uptime8",
+    name: "uptime",
     aliases: ["upt8"],
     version: "1.0",
     author: "JARiF@Cock",
